b4ffb045633a8a2a8ffb5432cfd26d873d07f7b9
Naga-Manohar-Y <61689114+Naga-Manohar-Y@users.noreply.github.com>
2023-09-08 02:49:44 -0400

    Update streamlit_app.py
    added url error lib

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-import pandas
 import streamlit
+import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 
-import requests
+
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -34,7 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Displaying that dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 #snowflake connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
